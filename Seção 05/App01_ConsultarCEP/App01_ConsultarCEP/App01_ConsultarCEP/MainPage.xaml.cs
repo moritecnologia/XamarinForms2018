184723aee23d8f96cafeefc57d0478ff1f8f7434
Rogerio Miadaira <moritec@hotmail.com>
2019-05-12 21:24:53 -0300

    App01 - Concluído

---
+++ Seção 05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App01_ConsultarCEP.Servico.Modelo;
+using App01_ConsultarCEP.Servico;
+
+namespace App01_ConsultarCEP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            /*Se o BuscarCEP ficar com erro porque não atende ao padrão do EventHandler, precisando de 2 parâmetros */
+            BOTAO.Clicked += BuscarCEP;
+
+        }
+
+        /*Usar o using App01_ConsultarCEP.Servico.Modelo; e o using App01_ConsultarCEP.Servico que foi criado antes*/
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+
+            //TODO - Validação
+            string cep = CEP.Text.Trim();
+
+            if (isValidCEP(cep))//TODO - Validação
+            {
+                //TODO - Lógica do Programa
+
+                try//try-catch = Lida com as exceções ou erros, poderá fazer um teste comentando a parte do if(cep.Length != 8)
+                {
+                    Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+
+                    if (end != null)//Este verifica se o cep não está nulo obtido de ViaCEPServico.cs em if (end.cep == null) return null;
+                    {
+                        RESULTADO.Text = String.Format("Endereço: {2} de {3} {0}, {1}", end.localidade, end.uf, end.logradouro, end.bairro);
+                    }
+                    else
+                    {
+                        DisplayAlert("ERRO", "O endereço não foi encontrado para o CEP informado: " + cep, "OK");
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    DisplayAlert("ERRO CRÍTICO", e.Message, "OK");
+                }
+            }
+
+        }
+
+        //TODO - Validação
+        private bool isValidCEP(string cep)
+        {
+            bool valido = true;
+
+            if(cep.Length != 8)
+            {
+                //ERRO
+                DisplayAlert("ERRO", "CEP Inválido! O CEP deve conter 8 caracteres.", "OK");
+                valido = false;
+            }
+
+            int NovoCEP = 0;
+            //TryParse = converte a string em integer de 32-bits
+            if (!int.TryParse(cep,out NovoCEP))
+            {
+                //ERRO
+                DisplayAlert("ERRO", "CEP Inválido! O CEP deve ser composto apenas por números.", "OK");
+                valido = false;
+            }
+
+            return valido;
+        }
+    }
+}
---
+++ Seção 05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/Servico/ViaCEPServico.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App01_ConsultarCEP.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace App01_ConsultarCEP.Servico
+{
+    class ViaCEPServico
+    {
+        //Cria-se a propriedade privada do tipo string EnderecoURL que receberá o parâmetro em "{0}"
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        //Cria-se o método BuscarEnderecoViaCEP com o parâmetro cep
+        //Colocando abaixo Endereco, retornará um objeto do tipo Endereco, precisando utilizar 
+        //a clase using App01_ConsultarCEP.Servico.Modelo;
+        public static Endereco BuscarEnderecoViaCEP (string cep)
+        {
+            //A variável NovoEnderecoURL receberá o endereço, com o parâmetro cep enciado para a propriedade EnderecoURL
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            //Utiliza-se a classe ou namespace using System.Net;
+            //Assim será possível utilizar a classe WebClient
+            WebClient wc = new WebClient();
+            //Utilizando o método Síncrono DownloadString
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            //Precisará instalar a biblioteca do NuGet que é o Newtonsoft.Json e usar  using Newtonsoft.Json;
+            //Neste abaixo com ajuda do JsonConvert.DeserializeObject vai obter do Conteudo a informação e converter em um objeto do tipo endereço
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            //Para tratar caso em que o cep não retorna o endereço, como exemplo 01000000
+            if (end.cep == null) return null;
+
+            //Retorna a variável endereço
+            return end;
+        }
+
+    }
+}
